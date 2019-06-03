687c487789a7025d177e37d16fa93f25e2f6385e
AaronStahley <acstahle@asu.edu>
2019-06-02 22:45:11 -0700

    login form is hiden when user is logged in and presents a logout button and vice versa

--- full_stack_app/resolutions_app/imports/startup/server/register-api.js
+++ full_stack_app/resolutions_app/imports/startup/server/register-api.js
@@ -3,15 +3,19 @@ import { makeExecutableSchema } from "graphql-tools";
 import merge from "lodash/merge";
 import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
 import ResolutionResolvers from "../../api/resolutions/resolvers";
-//-
+import UsersSchema from "../../api/users/User.graphql";
+import UsersResolvers from "../../api/users/resolvers";
+
+//--
 const testSchema = `
 type Query { 
     hi: String
     resolutions: [Resolution]
+    user: User
 }
 `;
 
-const typeDefs = [testSchema, ResolutionsSchema];
+const typeDefs = [testSchema, ResolutionsSchema, UsersSchema];
 
 const testResolver = {
   Query: {
@@ -21,7 +25,7 @@ const testResolver = {
   }
 };
 
-const resolvers = merge(testResolver, ResolutionResolvers);
+const resolvers = merge(testResolver, ResolutionResolvers, UsersResolvers);
 
 const schema = makeExecutableSchema({
   typeDefs,
--- full_stack_app/resolutions_app/imports/ui/App.js
+++ full_stack_app/resolutions_app/imports/ui/App.js
@@ -6,20 +6,25 @@ import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import { withApollo } from "react-apollo";
 
-const App = ({ loading, resolutions, client }) => {
+const App = ({ loading, resolutions, client, user }) => {
   if (loading) return null;
   return (
     <div>
-      <button
-        onClick={() => {
-          Meteor.logout();
-          client.resetStore();
-        }}
-      >
-        Logout
-      </button>
-      <RegisterForm client={client} />
-      <LoginForm client={client} />
+      {user._id ? (
+        <button
+          onClick={() => {
+            Meteor.logout();
+            client.resetStore();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <div>
+          <RegisterForm client={client} />
+          <LoginForm client={client} />
+        </div>
+      )}
       <ResolutionForm />
       <ul>
         {resolutions.map(resolution => (
@@ -35,6 +40,9 @@ const resolutionsQuery = gql`
     resolutions {
       _id
       name
+    }
+    user {
+      _id
     }
   }
 `;
