76b1b3046a28748401d90528d337a7bb4ca5b982
AaronStahley <acstahle@asu.edu>
2019-06-04 14:56:57 -0700

    goals are now attached to a resolution id number and can be fetched by asking for resolution and then goals within that.

--- full_stack_app/resolutions_app/imports/api/resolutions/resolvers.js
+++ full_stack_app/resolutions_app/imports/api/resolutions/resolvers.js
@@ -1,11 +1,22 @@
 import Resolutions from "./resolutions";
+import Goals from "../goals/goals";
 
 export default {
   Query: {
-    resolutions(obj, args, { userId = null }) {
-      return Resolutions.find({ userId }).fetch();
+    resolutions(obj, args, { userId }) {
+      return Resolutions.find({
+        userId
+      }).fetch();
     }
   },
+
+  Resolution: {
+    goals: resolution =>
+      Goals.find({
+        resolutionId: resolution._id
+      }).fetch()
+  },
+
   Mutation: {
     createResolution(obj, { name }, { userId }) {
       const resolutionId = Resolutions.insert({
