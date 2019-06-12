128d1473299e74bcb68e272bca6829311a55d3f7
Aaron Stahley <astahley@intersysconsulting.com>
2019-06-12 13:19:54 -0700

    finished tutorial

--- react-okta-staff-protal/src/App.js
+++ react-okta-staff-protal/src/App.js
@@ -4,17 +4,41 @@ import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+const config = {
+	issuer: "https://dev-981241.okta.com/oauth2/default",
+	redirect_uri: window.location.origin + "/implicit/callback",
+	client_id: "0oapoggqgrPWGbUcH356"
+};
+
+function onAuthRequired({ history }) {
+	history.push("/login");
+}
 
 function App() {
 	return (
 		<Router>
-			<div className="App">
-				<Navbar />
-				<div className="container">
-					<Route path="/" exact={true} component={Home} />
-					<Route path="/staff" exact={true} component={Staff} />
+			<Security
+				issuer={config.issuer}
+				client_id={config.client_id}
+				redirect_uri={config.redirect_uri}
+				onAuthRequired={onAuthRequired}
+			>
+				<div className="App">
+					<Navbar />
+					<div className="container">
+						<Route path="/" exact={true} component={Home} />
+						<SecureRoute path="/staff" exact={true} component={Staff} />
+						<Route
+							path="/login"
+							render={() => <Login baseUrl="https://dev-981241.okta.com" />}
+						/>
+						<Route path="/implicit/callback" component={ImplicitCallback} />
+					</div>
 				</div>
-			</div>
+			</Security>
 		</Router>
 	);
 }
--- react-okta-staff-protal/src/components/pages/Staff.js
+++ react-okta-staff-protal/src/components/pages/Staff.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Staff() {
-	return (
-		<div>
-			<h1>Staff</h1>
-		</div>
-	);
+class Staff extends Component {
+	state = {
+		currentUserName: "",
+		currentUserEmail: ""
+	};
+
+	componentDidMount() {
+		const idToken = JSON.parse(localStorage.getItem("okta-token-storage"));
+		this.setState({
+			currentUserName: idToken.idToken.claims.name,
+			currentUserEmail: idToken.idToken.claims.email
+		});
+	}
+
+	render() {
+		console.log(this.state);
+		const { currentUserEmail, currentUserName } = this.state;
+		return (
+			<div>
+				<h1>Welcome {currentUserName}</h1>
+				<p>Email: {currentUserEmail}</p>
+				<p>You have reached the authorized staff area of the protal</p>
+			</div>
+		);
+	}
 }
+
+export default Staff;
