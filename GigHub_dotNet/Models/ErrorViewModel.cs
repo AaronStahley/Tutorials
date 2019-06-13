eabe4aaa855748b07ffc50f27d31c3d3e97fbf9d
Aaron Stahley <astahley@intersysconsulting.com>
2019-06-13 11:04:31 -0700

    initial commit

---
+++ GigHub_dotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GigHub_dotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
