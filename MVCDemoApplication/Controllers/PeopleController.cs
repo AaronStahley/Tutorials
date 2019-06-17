87e9a38b5c2efff9281c2aa5418143d65359ffe3
Aaron Stahley <astahley@intersysconsulting.com>
2019-06-17 11:57:19 -0700

    fixed typo in dir and added new MVCDemoApplication training

---
+++ MVCDemoApplication/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using MVCDemoApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemoApplication.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age; 
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Aaron", LastName = "Stahley", Age = 24 });
+            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 34 });
+            people.Add(new PersonModel { FirstName = "Kristy", LastName = "Wickizer", Age = 26 });
+
+            return View(people); 
+        }
+    }
+}
